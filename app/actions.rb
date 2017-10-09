ed2cbc39bc0a3b210d28c4902442e83439c1870c
John Russell <jruss600@gmail.com>
2017-10-09 16:00:07 +0000

    files after week 5

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)   
@@ -19,9 +25,33 @@ post '/signup' do
                         password: password})
         
     if @user.save
-        "User #{username} has signed up successfully!"
+        redirect to ('/login')
     else
         erb(:signup)
     end
     
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+   username = params[:username]
+   password = params[:password]
+   
+   user = User.find_by(username: username)
+  
+    if user && user.password == password
+        session[:user_id] = user.id
+       redirect to ('/')
+    else
+       @error_message = "Login failed"
+       erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to ('/')
 end
