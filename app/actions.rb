bc511190082e5b2f8fcbd8549ed33ce60bb2e8fb
John Russell <jruss600@gmail.com>
2017-10-09 18:02:57 +0000

    files after w5d2 homework

--- app/actions.rb
+++ app/actions.rb
@@ -55,3 +55,29 @@ get '/logout' do
     session[:user_id] = nil
     redirect to ('/')
 end
+
+get '/posts/new' do
+    @post = Post.new
+   erb(:"posts/new") 
+end
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    
+    #instantiate new Post
+    @post = Post.new({ photo_url: photo_url, user_id: current_user.id})
+    
+    #if @post validates, save
+    
+    if @post.save
+        redirect(to('/'))
+    else
+        erb(:"posts/new")
+    end
+end
+
+get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb(:"posts/show")
+end
+
